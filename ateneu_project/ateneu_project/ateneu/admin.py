03eeed307ad199fbe483a172284d67614cfa39b5
pereirazul <inacio.teixeira09@aluno.ifce.edu.br>
2024-12-05 12:28:29 -0300

    Remoção de campos desnecessários do BD

--- ateneu_project/ateneu_project/ateneu/admin.py
+++ ateneu_project/ateneu_project/ateneu/admin.py
@@ -1,34 +1,24 @@
-
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-# Register your models here.
-# Personalizando a exibição do CustomUser no painel admin
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    # Campos que serão exibidos na lista de usuários no painel
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active')
-    # Campos que serão usados para pesquisa no painel
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    # Campos que serão usados no filtro lateral
+    list_display = ('email', 'username', 'is_staff', 'is_active')
+    search_fields = ('email', 'username')
     list_filter = ('is_staff', 'is_active')
-    # Campos que serão exibidos no formulário de edição de um usuário
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        (None, {'fields': ('email', 'username', 'password')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
-    # Campos que serão exibidos ao adicionar um novo usuário
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'username', 'password1', 'password2', 'first_name', 'last_name', 'is_staff', 'is_active'),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active'),
         }),
     )
-    # Configuração para exibir os campos de forma ordenada
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions')
 
-# Registra o modelo CustomUser com o CustomUserAdmin
 admin.site.register(CustomUser, CustomUserAdmin)
